3740205955cc433a371b4f9cd42858af065db02d
Marcus Andre Genorga <75354620+Andre113001@users.noreply.github.com>
2024-01-09 01:51:35 +0800

    Update db.js

--- database/db.js
+++ database/db.js
@@ -18,8 +18,8 @@ db.connect((err) => {
             host: process.env.MYSQL_HOST,
             port: process.env.MYSQL_PORT,
             user: process.env.MYSQL_USER,
-            password: process.env.MYSQL_PASS,
-            database: process.env.MYSQL_DB,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE,
         })
         console.error('Error connecting to MySQL:', err)
     } else {
