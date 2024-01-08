1c3e0808603b4b8e8f7d5d5e84b527e75289a7da
Marcus Andre Genorga <75354620+Andre113001@users.noreply.github.com>
2024-01-09 01:41:37 +0800

    Update db.js

--- database/db.js
+++ database/db.js
@@ -12,14 +12,19 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
     if (err) {
-        console.error('Error connecting to MySQL:', err);
+        console.log({
+            host: process.env.MYSQL_HOST,
+            port: process.env.MYSQL_PORT,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASS,
+            database: process.env.MYSQL_DB,
+        })
+        console.error('Error connecting to MySQL:', err)
     } else {
-        console.log(`======================================`);
-        console.log('Connected to MySQL database');
-        console.log(`Connected to MySQL database host: ${host}`);
-        console.log(`======================================`);
+        console.log(`======================================`)
+        console.log('Connected to MySQL database')
+        console.log(`======================================`)
     }
-});
-
+})
 module.exports = db;
 
